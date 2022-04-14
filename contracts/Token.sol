edf8fdaaf879dbf2ce8dcb219d9e3dc3ce9e8c51
varun0727 <varun.upadhyay911@gmail.com>
2022-04-14 15:37:21 +0530

    token name change

--- contracts/Token.sol
+++ contracts/Token.sol
@@ -4,8 +4,8 @@ pragma solidity 0.8.11;
 
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 
-contract Token is ERC20 {
-    constructor() ERC20("Token", "TOK") {
-        _mint(msg.sender, 10**11);
+contract VestingToken is ERC20 {
+    constructor() ERC20("VestingToken", "VT") {
+        _mint(msg.sender, 10**9);
     }
 }
