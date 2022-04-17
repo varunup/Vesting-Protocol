0fd92a33214a490b7ef0348217e0324c185b1125
dev_varun <varun.upadhyay911@gmail.com>
2022-04-17 21:32:30 +0530

    Update VestingToken.sol

--- contracts/VestingToken.sol
+++ contracts/VestingToken.sol
@@ -6,10 +6,6 @@ import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 
 contract VestingToken is ERC20 {
     constructor() ERC20("VestingToken", "VT") {
-<<<<<<< HEAD:contracts/Token.sol
         _mint(msg.sender, 10**24);
-=======
-        _mint(msg.sender, 10**9);
->>>>>>> 13a5358127bb5c5b7b094a441d3ab2619090506c:contracts/VestingToken.sol
     }
 }
